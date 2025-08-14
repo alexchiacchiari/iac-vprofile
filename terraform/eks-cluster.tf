e33a26552df36921be120b936fc8c804c2a612e2
alexchiacchiari <alex.chiacchiarivdg@gmail.com>
2025-08-14 23:50:41 +0200

    Fix Syntax Error

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -15,20 +15,20 @@ module "eks" {
 
   eks_managed_node_groups = {
     one = {
-      name           = "node-group-1"
-      instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      name                  = "node-group-1"
+      instance_types        = ["t3.small"]
+      min_size              = 1
+      max_size              = 3
+      desired_size          = 2
       create_before_destroy = true
     }
 
     two = {
-      name           = "node-group-2"
-      instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
+      name                  = "node-group-2"
+      instance_types        = ["t3.small"]
+      min_size              = 1
+      max_size              = 2
+      desired_size          = 1
       create_before_destroy = true
     }
   }
